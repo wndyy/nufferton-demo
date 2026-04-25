db693eadbf4c533b349f31751db0c70e39c31370
Wendy <30420239+wndyy@users.noreply.github.com>
2026-04-25 15:36:29 -0400

    feat: generic tailwind header
    Added free tailwind header: https://tailwindcss.com/plus/ui-blocks/marketing/elements/headers#component-27e5f71ced91b88e3f6b59ca69033a83. Supports mobile and has stacked flyout menu. Had to install two new, open-sourced libraries using: npm install @headlessui/react @heroicons/react.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Nufferton Demo</h1>
-      <p>
-        This is a minimal Next.js starter. If you can read this, the app is
-        running.
-      </p>
-      <p>
-        Edit <code>src/app/page.tsx</code> and save — the page will hot-reload.
-      </p>
-    </main>
+    <>
+      <Header />
+      <main>
+        <h1 className="text-3xl font-bold underline">Nufferton Demo</h1>
+        <p>
+          This is a minimal Next.js starter. If you can read this, the app is
+          running.
+        </p>
+        <p>
+          Edit <code>src/app/page.tsx</code> and save — the page will hot-reload.
+        </p>
+      </main>
+    </>
   );
 }
