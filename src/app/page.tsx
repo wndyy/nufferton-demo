46112ae5000b7bcd0e844cd0bb108489fbdb39b6
Wendy <30420239+wndyy@users.noreply.github.com>
2026-04-25 16:36:02 -0400

    feat: product list underneath hero
    Added product list feature underneath hero: https://tailwindcss.com/plus/ui-blocks/ecommerce/components/product-lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import ProductList from "./components/ProductList";
 
 export default function Home() {
   return (
     <div className="relative">
       <Header />
       <Hero />
+      <ProductList />
     </div>
   );
 }
